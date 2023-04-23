38da3e310aa4e6dc655037e59e5f165400385be6
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-04-22 23:56:24 -0300

    refactor: simplifying checked property storage

--- js/form.js
+++ js/form.js
@@ -5,12 +5,10 @@ export const getData = (event, redirectPath) => {
 
   for (const input of event.target) {
     if (input.name && input.type !== 'checkbox') {
-      localStorage.setItem(input.name, input.value)
+      localStorage.setItem(input.name, input.value);
     }
     else if (input.type === 'checkbox') {
-      input.checked 
-      ? localStorage.setItem(input.name, true) 
-      : localStorage.setItem(input.name, false); 
+      localStorage.setItem(input.name, input.checked);
     }
   }
 
