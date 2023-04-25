516c830a50b13b10f49c2967cd046ebd64beb9b1
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-04-24 21:10:42 -0300

    feat: adding addMask function

---
+++ js/validations/input-mask.js
@@ -0,0 +1,78 @@
+const addMask = (targetInput, mask) => {
+  targetInput.addEventListener('input', event => {
+    event.target.value = mask(event.target.value);
+  })
+}
+
+const onlyLetters = value => {
+  return value
+  .replace(/[0-9\\><!|@#¨}'{"/?$%^&*)(+=,:;._-]+/g, "");
+}
+
+const onlyNumbers = value => {
+  return value.replace(/\D/g, "");
+}
+
+const email = value => {
+  return value
+  .replace(/[\\><!|#¨}'{"/?$%^&*)(+=,;:]+/g, "");
+}
+
+// (00) 00000-0000
+const phone = value => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "($1) $2")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/-(\d{4})(\d)/, "$1");
+}
+
+// 00/00/0000
+const date = value => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{4})(\d)/, "$1");
+}
+
+// 000.000.000-00
+const CPF = value => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+}
+
+// 00.000.000/0000-00
+const CNPJ = value => {
+  return value
+  .replace(/\D/g, "")
+  .replace(/(\d{2})(\d)/, "$1.$2")
+  .replace(/(\d{3})(\d)/, "$1.$2")
+  .replace(/(\d{3})(\d)/, "$1/$2")
+  .replace(/(\d{4})(\d{1,2})/, "$1-$2")
+  .replace(/(-\d{2})\d+?$/, "$1");
+  // .replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1 $2 $3/$4-$5")
+}
+
+// 00000-000
+const zipCode = value => {
+  return value
+  .replace(/\D/g, "")
+  .replace(/^(\d{5})(\d{3})+?$/, "$1-$2");
+}
+
+export const inputMask = {
+  addMask,
+  onlyNumbers,
+  onlyLetters,
+  email,
+  phone,
+  date,
+  CPF,
+  CNPJ,
+  zipCode
+}
