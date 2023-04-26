df6ca11c6b81d9a3005c87175acb9ede06204679
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-04-26 19:41:33 -0300

    fix: zip code mask

--- js/validations/input-mask.js
+++ js/validations/input-mask.js
@@ -54,15 +54,15 @@ const CNPJ = value => {
   .replace(/(\d{3})(\d)/, "$1.$2")
   .replace(/(\d{3})(\d)/, "$1/$2")
   .replace(/(\d{4})(\d{1,2})/, "$1-$2")
-  .replace(/(-\d{2})\d+?$/, "$1");
-  // .replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1 $2 $3/$4-$5")
+  .replace(/-(\d{2})\d+?$/, "$1");
 }
 
 // 00000-000
 const zipCode = value => {
   return value
   .replace(/\D/g, "")
-  .replace(/^(\d{5})(\d{3})+?$/, "$1-$2");
+  .replace(/(\d{5})(\d)/, "$1-$2")
+  .replace(/-(\d{3})\d+?$/, '$1');
 }
 
 export const inputMask = {
