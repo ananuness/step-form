e32fc81b60fb354f3e4100fbc5276601dd2af75a
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-04-25 23:32:28 -0300

    refactor: simplifying storage data and separating responsibilities

--- js/components/form.js
+++ js/components/form.js
@@ -1,13 +1,16 @@
 export const redirect = (path) => window.location = path;
 
-// export const getData = (event) => {
-  
-// }
+export const getData = (event) => {
+  const data = {};
 
-export const storageData = (event) => {
   for (const input of event.target) {
     if (input.name && input.name !== 'noHouseNumber') {
-      localStorage.setItem(input.name, input.value);
+      data[input.name] = input.value;
     }
   }
+
+  return data;
 }
+
+export const storageObject = (property, object) => 
+  localStorage.setItem(property, JSON.stringify(object));
