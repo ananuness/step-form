91e868faada9a2dc4b5d4e2933114fb1ffd27d40
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-04-27 22:21:47 -0300

    feat: adding events to the payment page script

--- js/pages/payment.js
+++ js/pages/payment.js
@@ -1,5 +1,7 @@
-import { getData, redirect } from '../components/form.js';
+import { getData, redirect, storageObject } from '../components/form.js';
 import { updateStep } from '../components/stepper.js';
+import { inputMask } from '../validations/input-mask.js';
+import { inputValidation } from '../validations/inputs.js';
 
 const paymentForm = document.querySelector('.payment');
 const previousBtn = document.getElementById('prev-btn');
@@ -8,20 +10,86 @@ const cardNumberInput = document.getElementById('card-number');
 const cardHolderInput = document.getElementById('card-holder');
 const validityInput = document.getElementById('validity');
 const CVCInput = document.getElementById('cvc');
-const cardHolderCPFOrCNPJ = document.getElementById('card-holder-cpf-cnpj');
+const cardHolderCPFOrCNPJInput = document
+  .getElementById('card-holder-cpf-cnpj');
+
+const errorMessages = document.querySelectorAll('.input__error-message');
+const cardNumberError = document.getElementById('card-number-error');
+const cardHolderError = document.getElementById('card-holder-error');
+const validityError = document.getElementById('validity-error');
+const CVCError = document.getElementById('cvc-error');
+const cardHolderCPFOrCNPJError = document
+  .getElementById('card-holder-cpf-cnpj-error');
+
+const card = document.querySelector('.card-inner');
+const cardFlagImg = document.querySelector('.card__flag-img');
+const cardFlag = document.querySelector('.card__flag');
+const cardNumber = document.querySelector('.card__number');
+const cardName = document.querySelector('.card__name');
+const cardValidity = document.querySelector('.card__validity');
+const cardCVC = document.querySelector('.card__cvc');
 
 updateStep(3);
 
+const fillCardInfo = (cardField, value) => {
+  if (!value) {
+    cardField.textContent = cardField.getAttribute('data-label');
+    return;
+  }
 
-paymentForm.addEventListener('submit', event => {
-  event.preventDefault();
+  cardField.textContent = value;
+}
 
-  getData(event, './resume.html') // storageData
+cardNumberInput.addEventListener('input', event => {
+  // alterar flag e flag img aqui
+  fillCardInfo(cardNumber, event.target.value);
+  inputValidation.requiredFieldFilled(event, cardNumberError);
+});
 
-  // redirect
+cardHolderInput.addEventListener('input', event => {
+  fillCardInfo(cardName, event.target.value);
+  inputValidation.requiredFieldFilled(event, cardHolderError);
+});
+
+validityInput.addEventListener('input', event => {
+  fillCardInfo(cardValidity, event.target.value);
+  inputValidation.requiredFieldFilled(event, validityError);
+});
+
+CVCInput.addEventListener('input', event => {
+  fillCardInfo(cardCVC, event.target.value);
+  inputValidation.requiredFieldFilled(event, CVCError);
+});
+
+CVCInput.addEventListener('focusin', () => {
+  card.style.transform = 'rotateY(-180deg)';
+});
+
+CVCInput.addEventListener('focusout', () => {
+  card.style.transform = 'none';
+});
+
+cardHolderCPFOrCNPJInput.addEventListener('change', event => {
+  inputValidation.requiredFieldFilled(event, cardHolderCPFOrCNPJError);
 });
 
 previousBtn.addEventListener('click', event => {
   event.preventDefault();
+
+  alert('Os dados serão perdidos, tem certeza que quer voltar?');
+
   redirect('./address.html');
+});
+
+paymentForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!inputValidation.allRequiredFieldsFilled(event, errorMessages)) {
+    return;
+  }
+
+  return;
+  // getData(event, './resume.html') // storageData
+
+  // redirect
 });
