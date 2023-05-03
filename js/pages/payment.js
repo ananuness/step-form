380fd4b54e88900e2372e4fb9d4536f5100e62b4
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-05-03 20:53:10 -0300

    feat: adding all the functioning and animations of the payment page

--- js/pages/payment.js
+++ js/pages/payment.js
@@ -1,5 +1,6 @@
-import { getData, redirect, storageObject } from '../components/form.js';
+import { getData, redirect, storageData } from '../components/form.js';
 import { updateStep } from '../components/stepper.js';
+import { verifyCardType } from '../validations/card-flags.js';
 import { inputMask } from '../validations/input-mask.js';
 import { inputValidation } from '../validations/inputs.js';
 
@@ -23,15 +24,19 @@ const cardHolderCPFOrCNPJError = document
 
 const card = document.querySelector('.card-inner');
 const cardFlagImg = document.querySelector('.card__flag-img');
-const cardFlag = document.querySelector('.card__flag');
+const cardFlagName = document.querySelector('.card__flag');
 const cardNumber = document.querySelector('.card__number');
 const cardName = document.querySelector('.card__name');
 const cardValidity = document.querySelector('.card__validity');
 const cardCVC = document.querySelector('.card__cvc');
 
+let cardType = {};
+
 updateStep(3);
 
-inputMask.addMask(cardNumberInput, inputMask.card16Digit);
+inputMask.addMask(cardHolderInput, inputMask.onlyLetters);
+inputMask.addMask(CVCInput, inputMask.onlyNumbers);
+inputMask.addMask(validityInput, inputMask.monthYearDate);
 
 const fillCardInfo = (cardField, value) => {
   if (!value) {
@@ -42,26 +47,71 @@ const fillCardInfo = (cardField, value) => {
   cardField.textContent = value;
 }
 
+const setCardFeatures = (event) => {
+  const inputValue = event.target.value;
+
+  fillCardInfo(cardNumber, inputValue);
+
+  if (inputValue.length < 7) {
+    card.setAttribute('class', 'card-inner');
+    cardFlagImg.src = '../assets/flags/default.svg';
+    cardFlagName.textContent = cardFlagName.getAttribute('data-label');
+  }
+
+  cardType = verifyCardType(inputValue.slice(0, 8).replace(' ', ''));
+
+  if (cardType) {
+    card.setAttribute('class', 'card-inner');
+    card.classList.add(cardType.name);
+    cardFlagImg.src = `../assets/flags/${cardType.name}.svg`;
+    cardFlagName.textContent = `${cardType.formalName} • Crédito`;
+    cardNumberInput.maxLength = cardType.maxLengthWithSpaces;
+    cardHolderInput.maxLength = cardType.nameMaxLength;
+    CVCInput.maxLength = cardType.cvcLength;
+  }
+  else {
+    cardNumberInput.maxLength = 23;
+    cardHolderInput.maxLength = 22;
+    CVCInput.maxLength = 4;
+  }
+}
+
 cardNumberInput.addEventListener('input', event => {
-  // alterar flag e flag img aqui
-  fillCardInfo(cardNumber, event.target.value);
-  inputValidation.requiredFieldFilled(event, cardNumberError);
+  event.target.value = inputMask.cardNumber(
+    event.target.value, 
+    cardNumberInput.maxLength
+  );
+
+  setCardFeatures(event);
 });
 
-cardHolderInput.addEventListener('input', event => {
-  fillCardInfo(cardName, event.target.value);
-  inputValidation.requiredFieldFilled(event, cardHolderError);
-});
+cardNumberInput.addEventListener('change', event => 
+  inputValidation.cardNumber(cardType, event, cardNumberError)
+);
 
-validityInput.addEventListener('input', event => {
-  fillCardInfo(cardValidity, event.target.value);
-  inputValidation.requiredFieldFilled(event, validityError);
-});
+cardHolderInput.addEventListener('input', event =>
+  fillCardInfo(cardName, event.target.value.toUpperCase())
+);
 
-CVCInput.addEventListener('input', event => {
-  fillCardInfo(cardCVC, event.target.value);
-  inputValidation.requiredFieldFilled(event, CVCError);
-});
+cardHolderInput.addEventListener('change', event => 
+  inputValidation.name(event, cardHolderError)
+);
+
+validityInput.addEventListener('input', event =>
+  fillCardInfo(cardValidity, event.target.value)
+);
+
+validityInput.addEventListener('change', event => 
+  inputValidation.monthYearDate(event, validityError)
+);
+
+CVCInput.addEventListener('input', event =>
+  fillCardInfo(cardCVC, event.target.value)
+);
+
+CVCInput.addEventListener('change', event => 
+  inputValidation.CVC(event, CVCError)
+);
 
 CVCInput.addEventListener('focusin', () => 
   card.style.transform = 'rotateY(-180deg)'
@@ -71,14 +121,20 @@ CVCInput.addEventListener('focusout', () =>
   card.style.transform = 'none'
 );
 
-cardHolderCPFOrCNPJInput.addEventListener('change', event => {
-  inputValidation.requiredFieldFilled(event, cardHolderCPFOrCNPJError);
-});
+cardHolderCPFOrCNPJInput.addEventListener('input', event => 
+  event.target.value = inputMask.CPFOrCNPJ(
+    event.target.value, event.target.value.length
+  )
+);
+
+cardHolderCPFOrCNPJInput.addEventListener('change', event => 
+  inputValidation.CPFOrCNPJ(event, cardHolderCPFOrCNPJError)
+);
 
 previousBtn.addEventListener('click', event => {
   event.preventDefault();
 
-  alert('Os dados serão perdidos, tem certeza que quer voltar?');
+  // mostrar modal ao inves de um alert
 
   redirect('./address.html');
 });
@@ -90,8 +146,7 @@ paymentForm.addEventListener('submit', event => {
     return;
   }
 
-  return;
-  // getData(event, './resume.html') // storageData
-
-  // redirect
+  storageData('paymentMethod', getData(event));
+  storageData('cardType', cardType.formalName);
+  redirect('./resume.html');
 });
