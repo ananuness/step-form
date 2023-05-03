c5a813276ad3d92e35d52e37d3913bc743211e7d
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-05-03 20:47:51 -0300

    feat: adding new masks and adjusting some regex

--- js/validations/input-mask.js
+++ js/validations/input-mask.js
@@ -1,12 +1,12 @@
 const addMask = (targetInput, mask) => {
-  targetInput.addEventListener('input', event => {
-    event.target.value = mask(event.target.value);
-  })
+  targetInput.addEventListener('input', event => 
+    event.target.value = mask(event.target.value)
+  );
 }
 
 const onlyLetters = value => {
   return value
-  .replace(/[0-9\\><!|@#¨}'{"/?$%^&*)(+=,:;._-]+/g, "");
+    .replace(/[0-9\\><!|@#¨}'{"/?$%^&*)(+=,:;._-]+/g, "");
 }
 
 const onlyNumbers = value => {
@@ -15,7 +15,7 @@ const onlyNumbers = value => {
 
 const email = value => {
   return value
-  .replace(/[\\><!|#¨}'{"/?$%^&*)(+=,;:]+/g, "");
+    .replace(/[\s\\><!|#¨}'{"/?$%^&*)(+=,;:]+/g, "");
 }
 
 // (00) 00000-0000
@@ -28,12 +28,20 @@ const phone = value => {
 }
 
 // 00/00/0000
-const date = value => {
+const fullDate = value => {
   return value
     .replace(/\D/g, "")
     .replace(/(\d{2})(\d)/, "$1/$2")
     .replace(/(\d{2})(\d)/, "$1/$2")
     .replace(/(\d{4})(\d)/, "$1");
+}
+
+// 00/00
+const monthYearDate = value => {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1/$2")
+    .replace(/(\d{2})(\d)/, "$1");
 }
 
 // 000.000.000-00
@@ -42,65 +50,67 @@ const CPF = value => {
     .replace(/\D/g, "")
     .replace(/(\d{3})(\d)/, "$1.$2")
     .replace(/(\d{3})(\d)/, "$1.$2")
-    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
-    .replace(/(-\d{2})\d+?$/, "$1");
+    .replace(/(\d{3})(\d)/, "$1-$2")
+    .replace(/(-\d{2})(\d)/, "$1");
 }
 
 // 00.000.000/0000-00
 const CNPJ = value => {
   return value
-  .replace(/\D/g, "")
-  .replace(/(\d{2})(\d)/, "$1.$2")
-  .replace(/(\d{3})(\d)/, "$1.$2")
-  .replace(/(\d{3})(\d)/, "$1/$2")
-  .replace(/(\d{4})(\d{1,2})/, "$1-$2")
-  .replace(/-(\d{2})\d+?$/, "$1");
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1/$2")
+    .replace(/(\d{4})(\d{1,2})/, "$1-$2")
+    .replace(/-(\d{2})(\d)/, "$1");
 }
 
 // 00000-000
 const zipCode = value => {
   return value
-  .replace(/\D/g, "")
-  .replace(/(\d{5})(\d)/, "$1-$2")
-  .replace(/-(\d{3})\d+?$/, '$1');
+    .replace(/\D/g, "")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/-(\d{3})\d+?$/, '$1');
 }
 
-// 0000 000000 0000
-const card14Digit = value => {
-  return value
-  .replace(/\D/g, "")
-  .replace(/^(\d{4})/, "$1 ")
-  .replace(/(\d{6})/, "$1 ")
-  .replace(/(\d{4})\d+?$/, "$1");
+const cardNumber = (value, lengthWithSpaces) => {
+  if (lengthWithSpaces === 16) { // 0000 000000 0000
+    return value
+      .replace(/\D/g, "")
+      .replace(/^(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{6})(\d{4})$/, "$1 $2");
+  }
+  else if (lengthWithSpaces === 17) { // 0000 000000 00000
+    return value
+      .replace(/\D/g, "")
+      .replace(/^(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{6})(\d{5})$/, "$1 $2");
+  }
+  else if (lengthWithSpaces === 19) { // 0000 0000 0000 0000
+    return value
+      .replace(/\D/g, "")
+      .replace(/^(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})\d+?$/, "$1");
+  }
+  else { // 0000 0000 0000 0000 000
+    return value
+      .replace(/\D/g, "")
+      .replace(/^(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})(\d)/, "$1 $2")
+      .replace(/(\d{4})(\d)/, "$1 $2");
+  }
 }
 
-// 0000 000000 00000
-const card15Digit = value => {
-  return value
-  .replace(/\D/g, "")
-  .replace(/(\d{4})/, "$1 ")
-  .replace(/(\d{6})/, "$1 ")
-  .replace(/(\d{5})\d+?$/, "$1");
-}
-
-// 0000 0000 0000 0000
-const card16Digit = value => {
-  return value
-  .replace(/\D/g, "")
-  .replace(/(\d{4})(\d)/, "$1 $2")
-  .replace(/(\d{4})(\d)/, "$1 $2")
-  .replace(/(\d{4})(\d)/, "$1 $2")
-  .replace(/(\d{4})\d+?$/, "$1");
-}
-
-// 0000 0000 0000 0000 000
-const card19Digit = value => {
-  return value
-  .replace(/\D/g, "")
-  .replace(/(\d{4})(\d)/, "$1 $2")
-  .replace(/(\d{4})(\d)/, "$1 $2")
-  .replace(/(\d{4})(\d)/, "$1 $2")
-  .replace(/(\d{4})(\d)/, "$1 $2");
+const CPFOrCNPJ = (value, length) => {
+  if (length > 14) {
+    return CNPJ(value);
+  }
+  else {
+    return CPF(value);
+  }
 }
 
 export const inputMask = {
@@ -109,12 +119,11 @@ export const inputMask = {
   onlyLetters,
   email,
   phone,
-  date,
+  fullDate,
+  monthYearDate,
   CPF,
   CNPJ,
   zipCode,
-  card14Digit,
-  card15Digit,
-  card16Digit,
-  card19Digit
+  cardNumber,
+  CPFOrCNPJ
 }
